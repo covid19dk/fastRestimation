9f8c86e1ead8597a9e0c9e0b2a845c7989e574e0
Søren Welling <sorenwelling@Srens-MacBook-Pro.local>
2020-05-06 11:23:14 +0200

    tidy plot

---
+++ scripts/read_daily_hosp.R
@@ -0,0 +1,20 @@
+library(data.table)
+library(plotly)
+library(magrittr)
+
+fdir="./data/SSI_daily_hosp/"
+fext = ".csv"
+
+fpaths = list.files(fdir,fext,full.names = T)
+names(fpaths) = list.files(fdir,fext) %>% (function(x) gsub(fext,"",x))
+dt.list = lapply(fpaths %>% sort,fread)
+
+#correct offset in raw data
+dt.list$r200505$V1 %<>% (function(x) x/64*63)
+
+#simple plot of all timelines
+p = plot_ly(data=dt,x=~V1,y=~V2, type = "scatter", mode="lines+markers",name=names(dt.list)[1])
+j=1; for(i in dt.list[-1]) p %<>% add_trace(data=i,x=~V1,y=~V2,name=names(dt.list)[j=j+1])
+p %<>% layout(xaxis=list(title="days since 1st march 2020"),yaxis=list(title="daily new hospitalizations")) 
+
+p
