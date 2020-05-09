fa7e7025bae221627f62e2677163206ce37d1a2c
Søren Welling <sorenwelling@Srens-MacBook-Pro.local>
2020-05-09 12:42:21 +0200

    update manually read data

--- scripts/read_daily_hosp.R
+++ scripts/read_daily_hosp.R
@@ -15,6 +15,7 @@ dt.list = lapply(fpaths,fread,col.names=c("diffday","dailyHosp"))
 
 firstDay = as.Date.character("200301",format="%y%m%d")
 dt.list.dates = as.Date.character(substr(names(fpaths),2,7) ,format="%y%m%d")
+names(dt.list.dates) = names(dt.list)
 lastDay = tail(dt.list.dates,1)
 ALLDays = seq(firstDay,lastDay,by = 1)
 
@@ -24,12 +25,13 @@ dt.list$r200505$diffday %<>% (function(x) x/64*63)
 #round numbers and insert missing zeros
 
 dt.list %<>% lapply(round)
-j=0; dt.list = lapply(dt.list, function(x,J)  {
+dt.list[] = lapply(names(dt.list), function(dt_name)  {
+  x = dt.list[[dt_name]]
   x[,Date := firstDay+diffday]
-  theseDays = seq(firstDay,dt.list.dates[j<<-j+1],by = 1)
+  theseDays = seq(firstDay,dt.list.dates[dt_name],by = 1)
   x = x[match(theseDays,x$Date),]
   x$Date = theseDays
-  x$dailyHosp[1] = 1
+  x$dailyHosp[1] = 1 
   x$dailyHosp[is.na(x$dailyHosp)] = 0
   #x$diffday[is.na(x$diffday)] = 0
   x$Cdate = as.character(x$Date)
@@ -44,6 +46,52 @@ write.csv(df,file = "./data/SSI_daily_hosp_processed/fulltable.csv")
 
 
 
+View(dft)
+X = do.call(rbind,lapply( head(seq_along(dt.list.dates),-5), function(i) {
+  this_date = dt.list.dates[i] 
+  these_dates  = this_date+(0:5)
+  this_cdate = as.character(this_date)
+  these_reports = names(dt.list.dates)[match(these_dates,dt.list.dates)]
+  x = df[this_cdate,these_reports]
+  names(x) = paste0("x",1:6)
+  x
+}))
+
+X$wday = wday(row.names(X))
+X$wd4 = abs(X$wday-4)
+X$wd67 = X$wday%in%6:7
+
+plot(X$wday,X$x3/X$x6)
+text(X$wday,X$x3/X$x6,row.names(X),cex=.6)
+plot(X$wday,X$wd4)
+
+m2 = lm(x6 ~ x2,X)
+plot(
+  predict(m2,X),
+  X$x6
+)
+
+m3 = lm(x6 ~ x3,X)
+plot(
+  predict(m3,X),
+  X$x6
+)
+
+m4 = lm(x6 ~ x4,X)
+plot(
+  predict(m4,X),
+  X$x6
+)
+
+m5 = lm(x6 ~ x5,X)
+plot(
+  predict(m5,X),
+  X$x5
+)
+
+
+
+
 #simple plot of all timelines
 p = plot_ly(data=dt.list[[1]],x=~Date,y=~dailyHosp, type = "scatter", mode="lines+markers",name=names(dt.list)[1])
 j=1; for(i in dt.list[-1]) p %<>% add_trace(data=i,x=~Date,y=~dailyHosp,name=names(dt.list)[(j<<-j+1)])
@@ -53,3 +101,8 @@ p %<>% layout(
 ) 
 p
 #saveWidget(p,"viewReportedHospitalizations.html")
+
+tail(dt.list,1)
+
+X$x2
+
